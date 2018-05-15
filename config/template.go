8b1cfedca615a231fcf21ef5f010418ce3252257
Jacob Vallejo <you@jahkeup.com>
2018-05-14 18:49:24 -0700

    Add gtf golang template helpers

--- config/template.go
+++ config/template.go
@@ -4,7 +4,12 @@ import (
 	"text/template"
 
 	"github.com/jahkeup/repost/pkg/emailfuncs"
+	"github.com/leekchan/gtf"
 )
+
+func init() {
+	gtf.Inject(funcMap)
+}
 
 var funcMap = template.FuncMap{
 	"emailName":   emailfuncs.Name,
